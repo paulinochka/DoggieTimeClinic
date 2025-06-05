3719685907bd5b60abc51d1584bbb754d53055fc
Paulinochka <mfpolina2006@gmail.com>
2025-06-05 19:28:23 +0300

    Добавление юнит тестов для проверки формата номера телефона.

---
+++ DoggieTimeClinic/unit_test.py
@@ -0,0 +1,44 @@
+
+class Phone_true_Test(unittest.TestCase):
+    def test_is_match1(self):
+        self.assertTrue(validate_phone("89319916462"))
+
+    def test_is_match2(self):
+        self.assertTrue(validate_phone("8 931991-64-62"))
+
+    def test_is_match3(self):
+        self.assertTrue(validate_phone("+7 931 99164-62"))
+
+    def test_is_match4(self):
+        self.assertTrue(validate_phone("84739574929"))
+
+    def test_is_match5(self):
+        self.assertTrue(validate_phone("+7 565677 67-67"))
+
+    def test_is_match6(self):
+        self.assertTrue(validate_phone("+7 (565)677 67-67"))
+
+    def test_is_match7(self):
+        self.assertTrue(validate_phone("+7(565) 677 67-67"))
+
+class Phone_false_Test(unittest.TestCase):
+    def test_is_not_match1(self):
+        self.assertFalse(validate_phone("79319916462"))
+
+    def test_is_not_match2(self):
+        self.assertFalse(validate_phone("9 931991-64-62"))
+
+    def test_is_not_match3(self):
+        self.assertFalse(validate_phone("+8-931 99164-62"))
+
+    def test_is_not_match4(self):
+        self.assertFalse(validate_phone("84 739574929"))
+
+    def test_is_not_match5(self):
+        self.assertFalse(validate_phone("+7 (565677 67-67"))
+
+    def test_is_not_match6(self):
+        self.assertFalse(validate_phone("+7 565_-677 67-67"))
+
+    def test_is_not_match7(self):
+        self.assertFalse(validate_phone("+7 ( 565 677 67-67"))
